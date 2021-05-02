6c418b1471086fa9c35abc8dcb1502b999c2c742
Valentin Ludu <ludu.valentin@yahoo.com>
2021-05-02 14:34:41 +0300

    feat: Improved actions

--- src/hooks/useDashboard.js
+++ src/hooks/useDashboard.js
@@ -1,32 +1,32 @@
 import { useState, useCallback } from "react";
 import { mockContacts } from "../utils/mocks/mockContacts";
 import { wait } from "../utils/wait";
+import { deleteContact, getContacts, updateContact } from "../contexts/actions";
 
 export const useDashboard = (state, dispatch) => {
-
     const [disabled, setDisabled] = useState(true);
     
     const user = state.user;
 
     const handleDelete = (id) => {      
-        wait(1000, id).then(data => {
-            dispatch({type: "DELETE_CONTACT", payload: data});
+        wait(1000, id).then(contactId => {
+            dispatch(deleteContact(contactId));
         }).catch(err => {
             console.log(err);
         });
     };
 
     const fetchContacts = useCallback(() => {
-        wait(1000, mockContacts).then(data => {
-            dispatch({ type: "GET_CONTACTS", payload: data });
+        wait(1000, mockContacts).then(contacts => {
+            dispatch(getContacts(contacts));
         }).catch(err => {
             console.log(err);
         });
     }, [dispatch]);
 
     const handleFormSubmit = (contact) => {
-        wait(1000, contact).then(data => {
-            dispatch({ type: "UPDATE_CONTACT", payload: data });
+        wait(1000, contact).then(contactInfo => {
+            dispatch(updateContact(contactInfo));
             setDisabled(true);
         }).catch(err => {
             setDisabled(false);
